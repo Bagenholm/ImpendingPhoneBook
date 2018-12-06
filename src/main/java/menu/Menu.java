87b841023a969062e2f5b868b2a40a19dfbfc7fa
filip <filip.christofferson@gmail.com>
2018-12-06 13:42:09 +0100

    Add menu method

--- src/main/java/menu/Menu.java
+++ src/main/java/menu/Menu.java
@@ -1,4 +1,35 @@
 package menu;
 
+import java.util.ArrayList;
+import java.util.logging.Handler;
+
 public class Menu {
+
+    Handler handler;
+
+    ArrayList<Selectable> menuOptions = new ArrayList<Selectable>();
+    ArrayList<String> menuChoice = new ArrayList<String>();
+
+    public void add(String s, Selectable selectable) {
+        menuChoice.add(s);
+        menuOptions.add(selectable);
+    }
+
+    public void show(){
+        for (int i = 0; i < menuChoice.size(); i++) {
+            System.out.println(i + 1 + ". " + menuChoice.get(i));
+        }
+    }
+
+    public int choice(){
+        System.out.println("Please choose your action");
+
+        return handler.input.verifyInt(1, menuChoice.size());
+    }
+
+
+    public void run(){
+        menuOptions.get(choice -1).execute();
+    }
+
 }
