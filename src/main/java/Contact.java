e5e2e479fb1c06d997c3e39cb53a2808c88467c7
Sebastian <sebastian.aronsson@iths.se>
2018-12-06 13:47:28 +0100

    Add getters & setters

--- src/main/java/Contact.java
+++ src/main/java/Contact.java
@@ -20,5 +20,52 @@ public class Contact implements Serializable {
 
     public Contact() {
     }
-    
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getBirthYear() {
+        return BirthYear;
+    }
+
+    public void setBirthYear(int birthYear) {
+        BirthYear = birthYear;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
