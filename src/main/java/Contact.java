bb0cb3553ae7102d8bc90b7d6a3c68531969710c
Sebastian <sebastian.aronsson@iths.se>
2018-12-06 13:21:59 +0100

    Add contact variables

--- src/main/java/Contact.java
+++ src/main/java/Contact.java
@@ -1,2 +1,12 @@
-public class Contact {
+import java.io.Serializable;
+
+public class Contact implements Serializable {
+
+    private String name;
+    private int phoneNumber;
+    private String address;
+    private int BirthYear;
+    private String note;
+    private String email;
+
 }
