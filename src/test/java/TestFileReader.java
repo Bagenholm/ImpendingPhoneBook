1b3e800ed55f110a542f258b850eb9de5803aae9
Adam <skiryoland@gmail.com>
2018-12-09 14:12:17 +0100

    add public class instead of class to testfw & testfr

--- src/test/java/TestFileReader.java
+++ src/test/java/TestFileReader.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class TestFileReader {
+public class TestFileReader {
 
     @Test
     void readFromJsonReturnsNullWhenNoFileMatch() {
--- src/test/java/TestFileWriter.java
+++ src/test/java/TestFileWriter.java
@@ -8,7 +8,7 @@ import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class TestFileWriter {
+public class TestFileWriter {
 
 
     @Test
