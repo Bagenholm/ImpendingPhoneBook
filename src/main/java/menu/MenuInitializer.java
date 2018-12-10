a5e72dd89364fbccf0d81c4a18b493e42096a62a
Patrik <bagenholm.patrik@hotmail.com>
2018-12-10 12:28:07 +0100

    Merge menuInit to master.

--- src/main/java/menu/MenuInitializer.java
+++ src/main/java/menu/MenuInitializer.java
@@ -1,4 +1,72 @@
 package menu;
 
+//Metod save() i Handler?
+//Metod saveNew() i Handler?
+//ContactFinder returnerar Contact? Ska ContactFinder ta parametrar?
+//Contact-variabel birthYear kanske ska vara birthDay istället? Och inte lagra som en int utan en kalender?
+
 public class MenuInitializer {
+
+    Contact c; //TODO: C=searched contact?
+
+    Menu mainMenu = new Menu();
+    Menu createMenu = new Menu();
+    Menu saveMenu = new Menu();
+    Menu loadMenu = new Menu();
+    Menu searchMenu = new Menu();
+    Menu editMenu = new Menu();
+    Menu quitMenu = new Menu();
+
+    public void initalizeMenus() {
+
+        //Main Menu
+        mainMenu.add("Search for contact.", searchMenu.run());
+        mainMenu.add("Create contact.", createMenu.run());
+        mainMenu.add("Edit contact.", editMenu.run());
+        mainMenu.add("Save.", () -> saveMenu.run());
+        mainMenu.add("Load.", () -> loadMenu.run());
+        mainMenu.add("Quit.", () -> quitMenu.run());
+
+        //Create Menu
+        createMenu.add("Back to main menu.", () -> mainMenu.run());
+        createMenu.add("Make new contact.", () -> { c = new Contact(); editMenu.runEntireMenu(); } ); //TODO: Add chosen contact to edit menu
+
+
+        //Save Menu
+        saveMenu.add("Save to existing file.", () -> handler.save());
+        saveMenu.add("Save to new file.", handler.saveNew() );
+        saveMenu.add("Back to main menu.", mainMenu.run());
+
+
+        //Load Menu
+        loadMenu.add("Load from file.", () -> handler.load());
+        loadMenu.add("Back to main menu.", mainMenu.run());
+
+
+        //Search Menu
+        searchMenu.add("Search by name.", () -> c = handler.contactFinder.searchByName());
+        searchMenu.add("Search by phone number", () -> c = handler.contactFinder.searchByPhoneNumber());
+        searchMenu.add("Search by email", () -> c = handler.contactFinder.searchByEmail());
+        searchMenu.add("Search by birthday", () -> c = handler.contactFinder.searchByBirthYear());
+        searchMenu.add("Search by note.", () -> c = handler.contactFinder.searchByNote());
+        searchMenu.add("Search by any / Free search", () -> c = handler.contactFinder.searcyByAny());
+        searchMenu.add("Back to main menu.", mainMenu.run());
+
+
+        //Edit Menu
+        editMenu.add("Back to main menu.", mainMenu.run());
+        editMenu.add("Edit contact name.", () -> c.setName(handler.input.verifyString())); //TODO: C=searched contact?
+        editMenu.add("Edit contact phone number.", () -> c.setPhoneNumber(handler.input.verifyInt()));
+        editMenu.add("Edit contact adress.", () -> c.setAdress(handler.input.verifyString));
+        editMenu.add("Edit contact email.", () -> c.setEmail(handler.input.verifyString));
+        editMenu.add("Edit contact note.", () -> c.setNote(handler.input.verifyString));
+        editMenu.add("Edit contact birthday.", () -> c.setBirthYear(handler.input.verifyInt));
+
+
+        //Quit Menu
+        quitMenu.add("Back to main menu.", mainMenu.run());
+        quitMenu.add("Save and quit.", () -> {handler.save(); System.exit(0);});
+        quitMenu.add("Quit without saving.", () -> System.exit(0));
+
+    }
 }
