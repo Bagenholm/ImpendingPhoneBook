72526ba324bcc1e513a8be50941ab701ae8d351d
Patrik <bagenholm.patrik@hotmail.com>
2018-12-11 20:11:20 +0100

    Add tests for InputManager. Fix verifyDate().

--- src/main/java/util/InputManager.java
+++ src/main/java/util/InputManager.java
@@ -7,66 +7,76 @@ public class InputManager {
 
     Scanner sc = new Scanner(System.in);
 
-    /** Takes input and returns if it's a digit. */
+    public String takeInput() {
+        return sc.nextLine();
+    }
+
+    /** Takes input and returns if it's a digit trough verifyIntFormat(). */
     public int verifyInt() {
-        String input = sc.nextLine();
+        System.out.println("Please enter a digit.");
+        return verifyIntFormat(takeInput());
+    }
+
+    public int verifyIntFormat(String input) {
         int i;
         if( input.length() > 0 && input.matches("[0-9]*") ) {
             i = Integer.parseInt(input);
-
             return i;
         } else {
-            System.out.println("Invalid input, please enter a digit.");
             return verifyInt();
         }
     }
 
-    /** Takes input and returns if it's a digit between 0 and the max value.
+    /** Takes input and returns if it's a digit between 0 and the max value, through verifyInt(int, int, String).
      * Used to not get NullPointerException when accessing arraylists*/
     public int verifyInt(int min, int max) {
-        String input = sc.nextLine();
-        int i;
+        System.out.println("Please enter a digit.");
+        return verifyIntFormat(min, max, takeInput());
+    }
+
+    public int verifyIntFormat(int min, int max, String input) {
+        int i = Integer.parseInt(input);
         if( input.length() > 0 && input.matches("[0-9]*") ) {
-            i = Integer.parseInt(input);
             if (i >= min && i <= max)
                 return i;
             else return verifyInt(min, max);
         } else {
-            System.out.println("Invalid input, please enter a digit.");
             return verifyInt();
         }
     }
 
     /** For consistency, letting all input go through InputManager. */
     public String verifyString() {
-        return sc.nextLine();
+        return takeInput();
     }
 
     public LocalDate verifyDate() {
-        String year, month, day;
+        System.out.println("Please enter date in the format YYYYMMDD");
+        String input = takeInput();
+        return checkDateFormat(input);
+    }
 
-        String input = sc.nextLine();
+    public LocalDate checkDateFormat(String input) {
+        String year, month, day;
 
         if (input.length() == 8 && input.matches("[0-9]{8}")) {
             year = input.substring(0, 4);
             month = input.substring(4, 6);
             day = input.substring(6, 8);
-            return LocalDate.parse(year + month + day);
+            return LocalDate.parse(year + "-" + month + "-" + day);
         } else if (input.length() == 6 && input.matches("[0-9]{6}")) {
             if (Integer.parseInt(input.substring(0, 2)) > 19 ) {
                 year = "19"+input.substring(0, 2);
                 month = input.substring(2, 4);
                 day = input.substring(4, 6);
-                return LocalDate.parse(month + year + day);
+                return LocalDate.parse(year + "-" + month + "-" + day);
             } else {
                 year = "20"+input.substring(0, 2);
                 month = input.substring(2, 4);
                 day = input.substring(4, 6);
-                return LocalDate.parse(month + year + day);
+                return LocalDate.parse(year + "-" + month + "-" + day);
             }
-        }
-        else {
-            System.out.println("Please enter date in the format YYYYMMDD");
+        } else {
             return verifyDate();
         }
     }
